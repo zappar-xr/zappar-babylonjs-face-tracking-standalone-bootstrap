632be389decc036fea0e5234f4580ab35ef96ee9
cgauld <connell@freebreakfast.co.uk>
2021-06-21 12:30:21 +0000

    Deploying to gh-pages from  @ ff0eb6ffec0baa56ca8989e62192cd729f3982af 🚀

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
 
 const engine = new BABYLON.Engine(canvas, true, {
-  preserveDrawingBuffer: true,
-  stencil: true
+    preserveDrawingBuffer: true,
+    stencil: true
 });
 
 const scene = new BABYLON.Scene(engine);
@@ -15,8 +15,8 @@ const camera = new ZapparBabylon.Camera('camera', scene);
 
 // Request the necessary permission from the user
 ZapparBabylon.permissionRequestUI().then((granted) => {
-  if (granted) camera.start(true);
-  else ZapparBabylon.permissionDeniedUI();
+    if (granted) camera.start(true);
+    else ZapparBabylon.permissionDeniedUI();
 });
 
 // Set up our image tracker transform node
@@ -24,22 +24,22 @@ const faceTracker = new ZapparBabylon.FaceTrackerLoader().load();
 const trackerTransformNode = new ZapparBabylon.FaceTrackerTransformNode('tracker', camera, faceTracker, scene);
 
 const material = new BABYLON.StandardMaterial('mat', scene);
-material.diffuseTexture = new BABYLON.Texture("./faceMeshTemplate.png", scene);
+material.diffuseTexture = new BABYLON.Texture(("./faceMeshTemplate.png"), scene);
 
 // Face mesh
-const faceMesh = new ZapparBabylon.FaceMeshGeometry(scene);
+const faceMesh = new ZapparBabylon.FaceMeshGeometry('mesh', scene);
 faceMesh.parent = trackerTransformNode;
 faceMesh.material = material;
 
 
 
 window.addEventListener('resize', () => {
-  engine.resize();
+    engine.resize();
 });
 
 // Set up our render loop
 engine.runRenderLoop(() => {
-  faceMesh.updateFromFaceTracker(faceTracker);
-  camera.updateFrame();
-  scene.render();
+    faceMesh.updateFromFaceTracker(faceTracker);
+    camera.updateFrame();
+    scene.render();
 });
